9ae647e78ad47b4c222c2307bd5c218f43a8008e
shreyasajj <shreyas.github@proton.me>
2024-07-27 15:28:08 -0500

    penalized passed goals,/creates a goal list item

--- goallist.py
+++ goallist.py
@@ -1,5 +1,6 @@
 todolist_entity_id = data.get("entity_id")
-reset_window = data.get("reset_window", "weekly")
+current_time = datetime.datetime.now()
+reset_window = 7
 
 if todolist_entity_id is not None:
     # Get all todolist items
@@ -8,6 +9,42 @@ if todolist_entity_id is not None:
     logger.warning(all_goals)
 
     # Loop through all items
-    # for goal in all_goals[""]:
-    #     # Check for items not completed by due date
-    #     if 
+    for goal in all_goals[todolist_entity_id]["items"]:
+        # Skipping over values without description, summary, or status Not Supported
+        if not "description" in goal or not "status" in goal or len(goal["description"]) > 0:
+            logger.info("Skipping "+ goal["summary"] + " has no \"description\"/\"status\"")
+            continue
+        # Penalize goals passed dues 
+        penalize = False
+        if "due" in goal and goal["status"] == "needs_action":
+            # two different ways to define datetime
+            try:
+                goal_due = datetime.datetime.strptime(goal["due"], "%Y-%m-%dT%H:%M%S%z")
+            except ValueError:
+                goal_due = atetime.datetime.strptime(goal["due"], "%Y-%m-%d")
+            if current_time > goal_due:
+                penalize = True
+        
+        # Create a new goal template if it only contains a number and description
+        initial_description = goal["description"].split("\n")
+        error_budget_left = None
+        total_error_budget = None
+        remaining_days = reset_window - current_time.weekday()
+        if description[0].isdigit():
+            error_budget_left = int(description[0])
+            total_error_budget = error_budget_left
+            description.pop(0)
+        # if error_budget_left or total_error_budget is missing skip
+        if not error_budget_left or not total_error_budget:
+            logger.info(f"No \"Error Budget Left\" or \"Total Error Budget\" found in description for %s, skipping" % (goal["summary"]))
+            continue
+        # changing description 
+        description = f"Error Budget Left: %d\nTotal Error Budget: %d\nRemaining Days: %d\n" % (error_budget_left, total_error_budget, remaining_days)
+        description += '\n'.join(description)
+        service_data = {"entity_id": todolist_entity_id, "status":"needs_action", "item": goal["summary"], "due_date": current_time.strftime("%Y-%m-%d"), description: description}
+        hass.services.call("todo", "get_items", service_data, blocking=True, return_response=True)
+
+        
+
+
+        
