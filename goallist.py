fcbaf6c2db788e45e3d6b431d87b62aa4f139beb
shreyasajj <shreyas.github@proton.me>
2024-07-27 14:32:44 -0500

    testing

---
+++ goallist.py
@@ -0,0 +1,15 @@
+todolist_entity_id = data.get("enity_id")
+reset_window = data.get("reset_window", "weekly")
+
+if todolist_entity_id is None:
+    return 
+
+# Get all todolist items
+service_data = {"entity_id": todolist_entity_id}
+all_goals = hass.services.call("todo", "get_items", service_data, blocking=True, return_response=True)
+logger.warning(all_goals)
+
+# Loop through all items
+# for goal in all_goals[""]:
+#     # Check for items not completed by due date
+#     if 
